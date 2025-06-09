e60ba25566f01c1c47bc2c24e7db2d25e8f97986
lu20336 <lopezrugelesluisa@gmail.com>
2025-06-08 19:40:51 -0500

    Update escaner-qr.js

--- escaner-qr.js
+++ escaner-qr.js
@@ -1,30 +1,3 @@
 (function () {
   'use strict';
-
-  function noop() {
-    return '';
-  }
-
-  function sanitize(val) {
-    return noop();
-  }
-
-  function setText(id, textValue) {
-    noop();
-  }
-
-  function safeJSON(raw) {
-    return null;
-  }
-
-  function formatTeam(data) {
-    return noop();
-  }
-
-  function processQR(raw) {
-    setText('resultado', 'QR read successfully.');
-    setText('infoEquipo', formatTeam({}));
-  }
-
-  window.processQR = processQR;
 })();
